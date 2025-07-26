040f3f7bc6610fe8fe75e0195f4e11146b86781f
Leon Farafonov <117378906+leofaraf@users.noreply.github.com>
2025-07-26 10:56:01 +0300

    feat: add some traits data classes

---
+++ facetraits/traits.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+from enum import Enum
+
+@dataclass
+class Symmetry:
+    # Based on comparing distance of diff. parts of body
+    score: float
+
+class ChinType(Enum):
+    RECTANGULAR = "Rectangular"
+    POINTED = "Pointed"
+    ROUNDED = "Rounded"
+    RECESSED = "Recessed"
+    PROTRUDING = "Protruding"
+    UNKNOWN = "Unknown"
+
+@dataclass
+class Chin:
+    chin_width_to_face_width_ratio: float
+    chin_type: ChinType
+
+@dataclass
+class Jawline:
+    jawline_to_face_width_ratio: float
+
+@dataclass
+class Forehead:
+    face_height_ratio: float
+
+@dataclass
+class BrowRidges:
+    brow_eye_dist_to_face_height_ratio: float
